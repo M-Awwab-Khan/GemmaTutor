5926280f73cd8f0ebf1348a77a5f3a5af9fa965e
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-09-02 22:49:31 +0500

    modularity

--- app/page.js
+++ app/page.js
@@ -1,8 +1,35 @@
-import Image from "next/image";
-import { LandingPage } from "@/components/component/landing-page";
+'use client'
+import { useState, useEffect, useSyncExternalStore } from "react"
+import { Hero } from "@/components/component/hero";
+import { ChatInterface } from "@/components/component/chat-interface";
 
 export default function Home() {
-    return (
-        <LandingPage />
+    const [topic, setTopic] = useState('');
+    const [ageGroup, setAgeGroup] = useState('');
+    const [showResult, setShowResult] = useState(false);
+    const [sources, setSources] = useState([]);
+    const [isLoadingSources, setIsLoadingSources] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [userMessage, setUserMessage] = useState('');
+
+    return !showResult ? (
+        <Hero
+            topic={topic}
+            setTopic={setTopic}
+            ageGroup={ageGroup}
+            setAgeGroup={setAgeGroup}
+            setShowResult={setShowResult}
+        />
+    ) : (
+        <ChatInterface
+            messages={messages}
+            disabled={loading}
+            promptValue={userMessage}
+            setPromptValue={setUserMessage}
+            setMessages={setMessages}
+            topic={topic}
+        />
     );
+
 }
